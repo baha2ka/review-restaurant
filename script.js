3a091a092ce87df8151c9c77722b6f64e97badd5
BahaaKa <>
2021-06-30 11:57:09 +0000

    *2dsd2*

---
+++ script.js
@@ -0,0 +1,104 @@
+// 
+var username = 'baha2ka';
+var myId = 1234;
+var test = true;
+var x = 2;
+var m = 7;
+console.log(username);
+
+alert
+("WE ARE HERE TO HELP YOU "  +  username  );
+
+document.write
+("<p style='color:red;' > This article was written by a food lover </p>"); 
+
+var country = prompt ('where are you from?');
+console.log(country);
+if
+(country = " amman , jordan ")
+{alert("hello jordanian")}
+else
+{ alert("At the moment we do not serve your area")}
+//
+var favfoo = prompt("are you food lover?");
+
+favfood();
+
+function favfood(){
+    var valid = true;
+
+    while(valid){
+        var favfood = prompt("whats your fav food : american ,italian , chinese");
+         
+        if(favfood!="american" && favfood!="italian" && favfood!="chinese" ){
+          valid = true;
+}else{
+valid = false;
+return;
+}
+}
+}
+var foodfun = prompt("What star rating would you give 1-5");
+var rating = function(rating){
+
+
+for (var i=0;i<rating;i++){
+   document.write("<img src='https://martech.org/wp-content/uploads/2016/04/ss-rating-review-stars-800x450.jpg'/>"+"<p style='color:red;'> food lover "+(i+1)+"</p>") ;
+   console.log(i);
+}
+}
+rating(foodfun);
+
+
+
+
+
+
+
+
+
+
+
+// function favfood(){
+// while ( favfood != "american" && favfood != "italian")
+// { favfood = prompt (please enter on of :american ,italian , chinese);
+// }
+
+// // var x =prompt("how many times do you want to see a food image")
+// i
+// // var foodfun =function(){
+// iar output='';
+// // var m = 5;
+// var x = 2;
+// while (m < x) {
+// output = output + "<img src='https://i1.wp.com/www.eatthis.com/wp-content/uploads/2020/12/unhealthiest-foods-planet.jpg?resize=800%2C450&ssl=1' />"
+
+// console.log(m,output)
+
+// // m++; 
+
+// //   }
+// // return output;
+// // }
+// // var result = foodFun();
+// // document.write(result);
+
+
+
+// // var favfood = prompt("whats your fav food : american ,italian , chinese")
+
+// // food = prompt ("whats your fav food : american ,italian , chinese")
+
+// //  favfood=italian
+
+// //  function favfood() {
+
+// // while (favfood != "american" && favfood != "italian" && favfood != "chinese") {
+
+// // favfood = prompt("please enter on of : american, chinese, italian");
+
+
+
+
+
+
